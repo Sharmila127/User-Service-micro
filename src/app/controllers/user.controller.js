6f9bb4ba66691b13ae325b83cf577283bb91c860
Sharmila127 <sharmila6201@gmail.com>
2025-10-28 13:28:11 +0530

    Update user.controller.js

--- src/app/controllers/user.controller.js
+++ src/app/controllers/user.controller.js
@@ -22,3 +22,23 @@ export const profile = async (req, res, next) => {
     next(err);
   }
 };
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // Find user
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json({ message: 'User not found' });
+
+    // Check password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(401).json({ message: 'Invalid password' });
+
+    // Generate JWT token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+    res.status(200).json({ message: 'Login successful', token });
+  } catch (err) {
+    next(err);
+  }
+};
